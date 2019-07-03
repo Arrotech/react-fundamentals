a2abdb39dfcafeca565dc56ee6d6d64c98321811
Arrotech <arrotechdesign@gmail.com>
2019-07-04 02:33:12 +0300

    Chore (Components) Stateful Class Components
    What does this pr do?
    
    Create class components
    
    Any related pivotal stories?
    
    [Finishes #167082076]

---
+++ src/components/Arrotech.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+//Use stateful class components
+class Arrotech extends Component {
+    render () {
+        return <h1>Stateful Class Component</h1>
+    }
+
+}
+
+export default Arrotech
